27f5ebe9dea8b9b96b4fb7cc1baa569977a01895
bongsub kim <96857444+Bongseop-Kim@users.noreply.github.com>
2024-08-19 21:10:57 +0900

    Initial Commit

---
+++ Spark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spark
+//
+//  Created by 김봉섭 on 2024/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by 김봉섭 on 2024/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
